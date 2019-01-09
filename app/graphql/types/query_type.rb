d0bfdbc854a1aebf59b474b1b87de387bcfeb0e0
RickyTYL <rickytyl@icloud.com>
2019-01-09 19:58:42 +0800

    define Types::QueryType

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,23 @@
 module Types
   class QueryType < Types::BaseObject
+    name 'Query'
+
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :allMovies do
+      type types[Types::MovieType]
+      description 'All movies'
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+      resolve-> (obj, args, ctx) {
+        Movie.all
+      }
+    end
+
+    field :movie do
+      type Types::MovieType
+      description 'A movie'
+      argument :id, !types.ID
+      resolve-> (obj, args, ctx) { Movie.find(args[:id]) }
     end
   end
 end
