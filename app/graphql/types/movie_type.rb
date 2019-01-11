ad5905e291744db02fa97fc65191ebb3175d33db
RickyTYL <rickytyl@icloud.com>
2019-01-11 10:58:21 +0800

    upgrade legacy code

--- app/graphql/types/movie_type.rb
+++ app/graphql/types/movie_type.rb
@@ -1,9 +1,9 @@
 module Types
   class MovieType < Types::BaseObject
-    name "Movie"
+    graphql_name 'Movie'
 
-    field :id, !types.ID
-    field :title, !types.String
-    field :description, types.String
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :description, String, null: false
   end
 end
