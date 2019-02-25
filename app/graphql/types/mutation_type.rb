5e21685d780bdc30ee8d0e8f37db050baf97d8fc
RickyTYL <rickytyl@icloud.com>
2019-02-25 12:30:15 +0800

    add create review api

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -3,14 +3,28 @@ module Types
     graphql_name 'Mutation'
 
     field :create_movie, Types::MovieType, null: false do
+      description 'Add a movie'
       argument :title, String, required: true
       argument :description, String, required: true
+    end
+
+    field :add_a_review_for_the_movie, Types::ReviewType, null: false do
+      description 'Add a review for the movie'
+      argument :movie_id, ID, required: true
+      argument :content, String, required: true
     end
 
     def create_movie(title:, description:)
       Movie.create(
         title: title,
         description: description
+      )
+    end
+
+    def add_a_review_for_the_movie(movie_id:, content:)
+      Review.create(
+        movie_id: movie_id,
+        content: content
       )
     end
   end
