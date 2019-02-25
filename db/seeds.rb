64cb27e1a8d4f685ab5ca7848c6e32e816f02ca7
RickyTYL <rickytyl@icloud.com>
2019-02-25 12:30:44 +0800

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,18 @@
-5.times do
+movies = Faker::Movies.constants.select do |c|
+  Faker::Movies.const_get(c).is_a? Class
+end
+
+5.times do |i|
+  movie_id = i + 1
   Movie.create(
-    title: Faker::Name.name,
+    id: movie_id,
+    title: movies[i],
     description: Faker::Movie.quote
   )
+  3.times do
+    Review.create(
+      movie_id: movie_id,
+      content: Faker::Movies::HarryPotter.quote
+    )
+  end
 end
